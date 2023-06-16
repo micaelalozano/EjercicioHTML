91008e73089d93e8376c491a26a67d4ac3147f0d
micaelalozano <micaelalozano.16@hotmail.com>
2023-06-16 12:38:44 -0300

    Latest Update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
+import App from "./App";
+import "./index.css";
+import { Provider as StyletronProvider, DebugEngine } from "styletron-react";
+import { Client as Styletron } from "styletron-engine-atomic";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const debug =
+  process.env.NODE_ENV === "production" ? void 0 : new DebugEngine();
+
+const engine = new Styletron();
+
+ReactDOM.render(
+  <BrowserRouter>
+    <AuthProvider>
+      <StyletronProvider value={engine} debug={debug} debugAfterHydration>
+        <App />
+      </StyletronProvider>
+    </AuthProvider>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/pages/Inicio.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "../components/Header";
+
+const Inicio = () => {
+  return <>
+  <Header/>
+  <p>j</p>
+  </>;
+};
+
+export default Inicio;
